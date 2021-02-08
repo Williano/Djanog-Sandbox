53ff1761b1831c1181267563f5d9d85b053691d8
Williano <paawilly17@gmail.com>
2021-02-07 23:16:13 -0600

    Update

--- todo-api/todos/urls.py
+++ todo-api/todos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ListTodo, DetailTodo
 
+app_name = 'todo_api'
+
 urlpatterns = [
     path('<int:pk>/', DetailTodo.as_view(), name='DetailTodoAPIView'),
     path('', ListTodo.as_view(), name='ListTodoAPIView'),
