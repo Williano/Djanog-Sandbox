54aaf646ccc2fb22c43cd5fce841d05e7744375d
Williano <paawilly17@gmail.com>
2021-02-07 22:41:06 -0600

    Add database for todos app

--- todo-api/todos/models.py
+++ todo-api/todos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Todo(models.Model):
+
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
