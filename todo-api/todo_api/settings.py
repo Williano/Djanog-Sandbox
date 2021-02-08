032bc461be054ea4957d0895fe87302dd8b7d17e
Williano <paawilly17@gmail.com>
2021-02-07 22:33:28 -0600

    Add todos app to project

--- todo-api/todo_api/settings.py
+++ todo-api/todo_api/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # Default Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party django apps
+
+    # Project apps
+    'todos',
 ]
 
 MIDDLEWARE = [
