70059614eb46884a0aacef22f739eb438aa166ec
Williano <paawilly17@gmail.com>
2021-02-13 13:44:26 -0600

    Add test to test body content

--- todo-api/todos/tests.py
+++ todo-api/todos/tests.py
@@ -13,4 +13,8 @@ class TodoModelTest(TestCase):
         expected_object_name = f'{todo.tile}'
         self.assertEqual(expected_object_name, 'first todo')
 
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEqual(expected_object_name, 'a body here')
 
