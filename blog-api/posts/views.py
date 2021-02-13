a2d6b242afdd2a85ac23e2635c76e073d2da8823
Williano <paawilly17@gmail.com>
2021-02-13 16:07:14 -0600

    Add custom permission to page

--- blog-api/posts/views.py
+++ blog-api/posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
@@ -9,5 +10,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
