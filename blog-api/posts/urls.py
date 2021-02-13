d028e14c32eb3de0b038cad23c98eb6d1961b07f
Williano <paawilly17@gmail.com>
2021-02-13 15:03:34 -0600

    Add urls

---
+++ blog-api/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostList, PostDetail
+
+app_name = 'blog-api'
+
+urlpattern = [
+    
+    path('<int:pk>/', PostDetail.as_view(), name="post-detail"),
+    path('', PostList.as_view(), name="post-list")
+]
