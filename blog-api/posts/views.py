2f222bb7d65784c90138e3f17b71e80d82a1461d
Williano <paawilly17@gmail.com>
2021-02-13 15:56:07 -0600

    Update

--- blog-api/posts/views.py
+++ blog-api/posts/views.py
@@ -1,15 +1,13 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
