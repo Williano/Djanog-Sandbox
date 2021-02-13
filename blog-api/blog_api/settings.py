e66a56ade7f35bfe2b8e1c6cd931d8b118b56cdc
Williano <paawilly17@gmail.com>
2021-02-13 14:57:37 -0600

    Add restframework settings

--- blog-api/blog_api/settings.py
+++ blog-api/blog_api/settings.py
@@ -126,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django REST framework configuration
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
