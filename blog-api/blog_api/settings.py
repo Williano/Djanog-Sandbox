7d713e3b0b5123c844c5e681a83661d9afb9d704
Williano <paawilly17@gmail.com>
2021-02-13 14:55:44 -0600

    Add restframework package

--- blog-api/blog_api/settings.py
+++ blog-api/blog_api/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third-party django apps
+    'rest_framework',
 
     # Project apps
-    'posts'
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
